a8d4e2e5a995948dacca1959f5360578eba3d6cc
Jackie Tung <jackietung3@gmail.com>
2022-02-23 16:30:27 -0500

    commit tree ff5cc8e5e565f8510d2b8865a6172994026c1c12

---
+++ index.js
@@ -0,0 +1,16 @@
+function receivesAFunction(Spy) {
+    Spy();
+}
+
+
+function returnsANamedFunction() {
+    return function a(){
+
+    }
+};
+
+function returnsAnAnonymousFunction() {
+    return function(){
+        
+    }
+}
